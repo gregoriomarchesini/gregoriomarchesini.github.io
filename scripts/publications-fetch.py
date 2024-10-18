aec20b4e6be784fcc0be1f254ef73a62b50d2a7e
gregorio <gremar@kth.se>
2024-10-18 07:24:43 +0200

    fix: fix

--- scripts/publications-fetch.py
+++ scripts/publications-fetch.py
@@ -18,9 +18,13 @@ for pub in publications:
     publication_data.append({
         "name": pub['bib']['author'],
         "title": pub['bib']['title'],
+        "date": pub['bib']['pub_year'],
         "urlpdf": pub.get('pub_url', ''),  # URL if available
         "urlbib": f"google_scholar_bibtex_link_{pub['bib']['title']}",  # Needs manual export or additional scraping
     })
+
+# order publications by date
+publication_data = sorted(publication_data, key=lambda x: x['date'], reverse=True)
 
 # Save or export the publication data as yaml
 with open('publications.yaml', 'w') as f:
