d654cd6aa3aecd0833d4faf4d37e675096228688
gregoriomarchesini <71294988+gregoriomarchesini@users.noreply.github.com>
2025-05-01 18:24:04 +0000

    Deploying to gh-pages from @ gregoriomarchesini/gregoriomarchesini.github.io@d53764d06b3774df14f8d751cb3291ecf7e6a051 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,9 +30,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+        },{id: "nav-my-github-projects",
+          title: "My Github Projects",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
